0c1d31dfc61efb6ccfdbc3a7073e17b08e25727d
Rupesh Kumar Sahu <rupeshksahu7@gmail.com>
2020-05-17 09:21:25 +0530

    second commit

--- app/src/main/java/com/codeprim/loginapp/ui/LoginActivity.kt
+++ app/src/main/java/com/codeprim/loginapp/ui/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.codeprim.loginapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
@@ -9,13 +10,31 @@ import com.codeprim.loginapp.R
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var usernameTxt : EditText
-    private lateinit var emailTxt : EditText
     private lateinit var passwordTxt : EditText
-    private lateinit var submitBtn : TextView
+    private lateinit var loginBtn : TextView
+    private lateinit var createProfileTxt : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        viewAction()
+    }
 
+    private fun viewAction() {
+        usernameTxt = findViewById(R.id.usernameTxt)
+        passwordTxt = findViewById(R.id.passwordTxt)
+        loginBtn = findViewById(R.id.loginBtn)
+        createProfileTxt = findViewById(R.id.createProfileTxt)
+
+        loginBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        createProfileTxt.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/codeprim/loginapp/ui/SignupActivity.kt
+++ app/src/main/java/com/codeprim/loginapp/ui/SignupActivity.kt
@@ -1,13 +1,43 @@
 package com.codeprim.loginapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import com.codeprim.loginapp.R
 
 class SignupActivity : AppCompatActivity() {
 
+    private lateinit var usernameTxt : EditText
+    private lateinit var emailTxt : EditText
+    private lateinit var passwordTxt : EditText
+    private lateinit var submitBtn : TextView
+    private lateinit var loginProfileTxt : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        viewAction()
+    }
+
+    fun viewAction() {
+        usernameTxt = findViewById(R.id.usernameTxt)
+        emailTxt = findViewById(R.id.emailTxt)
+        passwordTxt = findViewById(R.id.passwordTxt)
+        submitBtn = findViewById(R.id.submitBtn)
+        loginProfileTxt = findViewById(R.id.loginProfileTxt)
+
+        submitBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginProfileTxt.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
